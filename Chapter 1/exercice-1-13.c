0cbeabe365f38df341a3dfa4894bec1e5356f9aa
DianChen <chendian2017@gmail.com>
2021-05-24 11:17:44 +0200

    Create exercice-1-13.c
    Write a program to print a histogram of the lengths of words in its input.

---
+++ Chapter 1/exercice-1-13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define IN  1
+#define OUT 0
+
+int main(int argc, const char * argv[]) {
+    // Exercice 1-12. Write a program that prints its input one word per line
+    int c, state;
+    
+    state = OUT;
+
+    while ((c=getchar()) != EOF){
+
+        if (c == ' ' || c == '\t' || c == '\n')
+            state = OUT;
+        else if (state == OUT){
+            putchar('\n');
+            state = IN;}
+        if (state == IN){
+            putchar('*');
+        }
+    }
+    return 0;
+}
